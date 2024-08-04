04e591f6b7912bfcc90b843412dddbf84f12c49e
KUYATRE <grbgrnd1120@naver.com>
2024-08-04 21:30:20 +0900

    web view#1

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadWebPage("https://kloa.gg")
     }
 
-
+    @IBAction func btnGoToUrl(_ sender: Any) {
+    }
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
 }
 
